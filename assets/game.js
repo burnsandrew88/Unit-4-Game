82662cddbe81a3d716340f84f4d3260d9db87c01
Andrew Burns <andrewburns@stp-er-10-129-120-20.wireless.umn.edu>
2018-10-10 21:35:38 -0500

    New CSS Design

---
+++ assets/game.js
@@ -0,0 +1,37 @@
+// On Click Elements 
+
+$("#RedRupee").on(RedRupeeButton)
+$("#BlueRupee").on(BlueRupeeButton)
+$("#GreenRupee").on(GreenRupeeButton)
+$("#GoldRupee").on(GoldRupeeButton)
+
+// Game start Function
+
+
+// Red Rupee Function
+function RedRupeeButton(){
+    
+}
+
+// Blue Rupee Function
+function BlueRupeeButton(){
+    
+}
+
+// Green Rupee Function
+
+function GreenRupeeButton(){
+    
+}
+
+// Gold Rupee Function
+
+function GoldRupeeButton(){
+    
+}
+
+// If you win
+
+// If you lose
+
+// Game Reset Function
